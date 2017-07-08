b953983556cf2c7a4545a983a631b2903c2f03b8
Alec Schleicher <schleicher@3cisd.com>
2017-07-08 13:05:45 -0400

    login working

--- auth.js
+++ auth.js
@@ -10,14 +10,19 @@ var config = {
 firebase.initializeApp(config);
 
 
-var provider = new firebase.auth.FacebookAuthProvider();
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    console.log(result);
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-}).catch(function(error) {
-    console.log(error);
+
+$(".btn-facebook").click(function() {
+    // Create instance of Facebook provider object
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    // Login
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        console.log(result);
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function(error) {
+        console.log(error);
+    }); 
 });
