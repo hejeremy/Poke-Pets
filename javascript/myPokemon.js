b2526d379564dfeba61540b6893a37cff55dce3d
Caroline Bowes <carvbowes@gmail.com>
2017-07-10 22:57:43 -0400

    added ajax call in mypokemon.js

--- javascript/myPokemon.js
+++ javascript/myPokemon.js
@@ -25,12 +25,20 @@ $(document).ready(function() {
 
 		for (var i = 0; i<currentPlayer.pokemon.length; i++) {
 			var current = currentPlayer.pokemon[i];
+			var queryURL = "https://pokeapi.co/api/v2/pokemon-species/" + current.Name + "/";
+		   
+		   $.ajax({
+				url: queryURL,
+				method: "GET"
+			}).done(function(response) {
+				console.log(response);
+			});
 
 			var pokemonDiv = $("<div>").addClass("pokemon").attr("id", current.Name);
 			var container = $("<div>").addClass("col-xs-4 img-container");
 			var img = $("<img>").addClass("pokemon-img").attr("src", current.ImgLarge);
 			var lvl = $("<div>").addClass("pokemon-lvl text-center").html("<h4>XP: " + current.EXP + "</h4>");
-			var stats = $("<div>").addClass("pokemon-stats col-xs-8").html("<h6>?</h6><h4>" + current.Name + "</h4>");
+			var stats = $("<div>").addClass("pokemon-stats col-xs-8").html("<h6>no. " + + "</h6><h4>" + current.Name + "</h4>");
 
 			container.html(img).append(lvl);
 			pokemonDiv.html($("<div>").addClass("row").append(container).append(stats));
