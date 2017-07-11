be469d7a8c205cabc997ea939443d495d1e1a79b
Caroline Bowes <carvbowes@gmail.com>
2017-07-10 21:27:15 -0400

    incorporated ImageLarge property in mypokemon list

--- javascript/myPokemon.js
+++ javascript/myPokemon.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
 
 			var pokemonDiv = $("<div>").addClass("pokemon").attr("id", current.Name);
 			var container = $("<div>").addClass("col-xs-4 img-container");
-			var img = $("<img>").addClass("pokemon-img").attr("src", current.ImgLink);
+			var img = $("<img>").addClass("pokemon-img").attr("src", current.ImgLarge);
 			var lvl = $("<div>").addClass("pokemon-lvl text-center").html("<h4>XP: " + current.EXP + "</h4>");
 			var stats = $("<div>").addClass("pokemon-stats col-xs-8").html("<h6>?</h6><h4>" + current.Name + "</h4>");
 
